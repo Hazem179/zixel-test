42275cd35c33e5bef4fc9c202fe12ef6ba1f220c
Hazem179 <hazemm913@gmail.com>
2021-12-23 14:01:29 +0200

    first commit

---
+++ nlp/tags_extraction.py
@@ -0,0 +1,12 @@
+import spacy
+import spacy_ke
+
+# load spacy model
+nlp = spacy.load("en_core_web_md")
+nlp.add_pipe("yake")
+def tags_extraction(description):
+    tags = []
+    doc = nlp(description)
+    for keyword, score in doc._.extract_keywords(n=10):
+        tags.append(str(keyword))
+    return tags
